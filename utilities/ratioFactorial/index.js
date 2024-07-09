187762c45e08e5dae1661cf0115567895e86ade8
root <you@example.com>
2024-07-09 07:34:19 +0000

    loading

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,10 @@
+const ratioOfNumber = require("../ratio/index");
+const factorialOfNumber = require("../factorial/index");
+
+const ratioAndFactorial = (num1, num2, num3) => {
+  const ratio = ratioOfNumber(num1, num2);
+  const factorial = factorialOfNumber(num3);
+  return { ratio, factorial };
+};
+
+module.exports = ratioAndFactorial;
